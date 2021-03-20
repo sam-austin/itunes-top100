6a2922a1584345be731a2f0ffdf3fa3716968748
“Potato <samson.park1@gmail.com>
2021-03-20 19:10:47 -0400

    css animation, tiles flex wrapped

--- client/src/components/layout/AlbumTile.js
+++ client/src/components/layout/AlbumTile.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React from 'react'
 
 const AlbumTile = ({ fetchedAlbum, index }) => {
 
   return(
-    <div>
+    <div className='album-tile'>
       <h6>{index}</h6>
-      <img alt="album img" src={fetchedAlbum["im:image"][0].label} />
+      <img alt='album img' src={fetchedAlbum["im:image"][0].label} />
       <h5>{fetchedAlbum["im:name"].label}</h5>
       <h5>{fetchedAlbum["im:artist"].label}</h5>
     </div>
